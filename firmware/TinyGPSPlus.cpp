1ae9b039bf3d1173ba618965b0334b7742ace3fe
Bertrand Sirodot <bertrand_sirodot@leobigfoot.com>
2016-02-25 14:03:37 -0700

    Added math.h to TinyGPSPlus.cpp

--- firmware/TinyGPSPlus.cpp
+++ firmware/TinyGPSPlus.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define _GPRMCterm   "GPRMC"
 #define _GPGGAterm   "GPGGA"
